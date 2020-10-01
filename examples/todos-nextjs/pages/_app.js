3902df48a4f5d8446df17836ddb24c368bf2b154
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-10-01 22:11:34 +0200

    feat: Minimal Tailwindcss setup

--- examples/todos-nextjs/pages/_app.js
+++ examples/todos-nextjs/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import 'styles/style.scss'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps}>
+      <Head>
+        <title>Supabase Todo Example</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+  </Component>
 }
 
 export default MyApp
